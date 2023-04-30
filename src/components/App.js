6ebfea7cb263e575c7338c259dd9467ed1894c07
Narine <115227487+narinearut@users.noreply.github.com>
2023-04-30 16:05:03 +0100

    Profiles 1 task

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import Profile from './Profile.js';
+import user from '../user.json';
+
+export default function App() {
+  return (
+   <div>
+   <Profile 
+    username = {user.username}
+    tag = {user.tag}
+    location = {user.location}
+    avatar = {user.avatar}
+    stats = {user.stats}
+   />
+    </div>
+  )
+};
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
